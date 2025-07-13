59b7df871959169a997a42ba0981d2f62dbff93c
zainabalabbood <zainab2125p.zaa@gmail.com>
2025-07-13 16:09:41 +0300

    second comment

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// Here is where we import modules
-// We begin by loading Express
-const express = require('express');
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const app = express();
+// MIDDLEWARE
+app.use(express.static(path.join(__dirname, 'public')))
+
+// GET / (home)
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
-});
+    console.log('Listening on port 3000')
+})
